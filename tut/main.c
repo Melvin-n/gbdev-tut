cd2fd6d34ad58ae00834cf6e4d334308ac12ac14
melvin <melvin.narayan@9spokes.com>
2023-08-09 22:27:22 +1200

    bg test

--- tut/main.c
+++ tut/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <gb/gb.h>
+#include <demobg.h>
 
 void main()
 {
     DISPLAY_ON;
 
+    SHOW_BKG;
+
+    set_bkg_data(0, LaroldsJubilantJunkyard_tileCount, LaroldsJubilantJunkyard_data);
+    set_bkg_tiles(0, 0, 20, 18, LaroldsJubilantJunkyard_map_plane0);
     while (1)
     {
         wait_vbl_done();
